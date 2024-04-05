04615431233017f65af5218d18ca562a14bad520
convidat <convidat@aula-a-siee018.uab.es>
2024-04-05 18:05:25 +0200

    Afegides definicions de mètodes i variables internes

--- tetris/joc.h
+++ tetris/joc.h
@@ -1,2 +1,23 @@
 #ifndef JOC_H
 #define JOC_H
+
+Class Joc
+{
+public:
+	void inicialitza(const string& nomFitxer);
+	bool giraFigura(DireccioGir direccio);
+	bool mouFigura(int dirX);
+	int baixaFigura();
+	void escriuTauler(const string& nomFitxer);
+	//funcions nostres a partir d'aquí
+	void incrementaPuntuacio();
+	void incrementaNivell();
+private:
+	Tauler m_tauler;
+	Figura m_figura;
+	//
+	int puntuacio;
+	int nivell;
+	
+}
+
