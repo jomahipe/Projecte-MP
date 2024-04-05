2d378f792018c6648d92dc8d74ac30aed678be4a
convidat <convidat@aula-a-siee018.uab.es>
2024-04-05 18:26:25 +0200

    Afegits comentaris

--- tetris/joc.h
+++ tetris/joc.h
@@ -9,13 +9,14 @@ public:
 	bool mouFigura(int dirX);
 	int baixaFigura();
 	void escriuTauler(const string& nomFitxer);
-	//funcions nostres a partir d'aquí
+	//funcions nostres a partir d'aquí: qualsevol funció més que veiem útil o bona addició al programa (funcionalitats extra pugen nota)
+	//això ho implementem després de tenir el joc bàsic programat
 	void incrementaPuntuacio();
 	void incrementaNivell();
 private:
 	Tauler m_tauler;
 	Figura m_figura;
-	//
+	//coses nostres aquí
 	int puntuacio;
 	int nivell;
 	
