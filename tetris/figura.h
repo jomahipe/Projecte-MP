7e797ef6e1993a90db5b02d3a9a9b2248d66a07b
José Mª Hidalgo Pérez <josemaria.hidalgo@autonoma.cat>
2024-04-13 20:50:24 +0200

    Declaracions inicials de membres de classe i enums per controlar forma i color

--- tetris/figura.h
+++ tetris/figura.h
@@ -2,6 +2,38 @@
 #define FIGURA_H
 //codi aqui
 
+typedef enum
+{
+NO_FIGURA = 0,
+FIGURA_O,
+FIGURA_I,
+FIGURA_T,
+FIGURA_L,
+FIGURA_J,
+FIGURA_Z,
+FIGURA_S
+} TipusFigura;
 
+typedef enum
+{
+COLOR_NEGRE = 0,
+COLOR_GROC,
+COLOR_BLAUCEL,
+COLOR_MAGENTA,
+COLOR_TARONJA,
+COLOR_BLAUFOSC,
+COLOR_VERMELL,
+COLOR_VERD,
+NO_COLOR
+} ColorFigura;
+
+Class Figura
+{
+public:
+
+private:
+  TipusFigura m_tipusFigura;
+  ColorFigura m_colorFigura;
+}
 
 #endif
