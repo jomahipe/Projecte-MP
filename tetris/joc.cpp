a7086606a484c9a93893f38972380ca05353299b
José Mª Hidalgo Pérez <josemaria.hidalgo@autonoma.cat>
2024-04-18 20:35:19 +0200

    Començada implementació

--- tetris/joc.cpp
+++ tetris/joc.cpp
@@ -1,1 +1,26 @@
 #include "joc.h"
+using namespace std;
+
+void Joc::inicialitza(const string& nomFitxer)
+{
+	ifstream fitxer;
+	fitxer.open(nomFitxer);
+	if (fitxer.is_open())
+	{
+	/*
+	algorisme d'inicialització aquí
+	 */
+	}
+}
+
+void Joc::escriuTauler(const string& nomFitxer)
+{
+	ofstream fitxer;
+	fitxer.open(nomFitxer);
+	if (fitxer.is_open))
+	{
+	/*
+	 algorisme d'escriptura aquí
+	 */
+	}
+}
