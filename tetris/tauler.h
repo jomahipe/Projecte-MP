478f07bf037365c9ec96354c023fcc58bb307b8f
jomahipe <163568974+jomahipe@users.noreply.github.com>
2024-04-19 17:30:26 +0200

    Update tauler.h

--- tetris/tauler.h
+++ tetris/tauler.h
@@ -8,8 +8,7 @@ const int MAX_COLUMNES;
 class Tauler
 {
 public:
-bool movimentValid()// aquesta funcio l'he posat pero no se si es necessari
-	void eliminaFila();
+void eliminaFila();
 void actualitzarTaula(); //he afegit aquesta funcio en cas si eliminem una fila
 private:
  	 ColorFigura m_tauler[N_FILES][N_COLUMNES];
