c6da43deb2a3c82a8d77ab46fd53fd8f01b39c38
José María Hidalgo Pérez <josemaria.hidalgo@autonoma.cat>
2024-04-30 16:57:23 +0200

    Afegit getter

--- tetris/tauler.h
+++ tetris/tauler.h
@@ -10,7 +10,9 @@ class Tauler
 public:
 	Tauler() { m_tauler[N_FILES][N_COLUMNES] = {{NO_COLOR}}; }
 	Tauler(ColorFigura taulerInput); // constructor que inicialitza el tauler que rep com a paràmetre
-	void eliminaFila(); 
+	void eliminaFila();
+	//getters
+	ColorFigura getValor(int fila, int col) { return m_tauler[fila][col]; }
 private:
   	ColorFigura m_tauler[N_FILES][N_COLUMNES];
   
